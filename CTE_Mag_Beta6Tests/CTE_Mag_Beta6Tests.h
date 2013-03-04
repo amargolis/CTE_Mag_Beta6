c43fd724adaa1d2c71fe2f0aeaaa5f8e3a79a0c5
Andy Margolis <amargolis21@me.com>
2013-03-04 11:20:21 -0600

    Initial Commit

---
+++ CTE_Mag_Beta6Tests/CTE_Mag_Beta6Tests.h
@@ -0,0 +1,13 @@
+//
+//  CTE_Mag_Beta6Tests.h
+//  CTE_Mag_Beta6Tests
+//
+//  Created by Andy Margolis on 3/4/13.
+//  Copyright (c) 2013 Northshore Technology Services. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CTE_Mag_Beta6Tests : SenTestCase
+
+@end
